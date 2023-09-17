6db720c3aa1600f52fd2281da950ac6fa608783b
Estefanny Oliveira <estefannyoliveira453@gmail.com>
2023-09-16 22:26:37 -0400

    versão-final

--- index.js
+++ index.js
@@ -14,8 +14,35 @@ function relogio() {
   minutos.querySelector('p').textContent = minuto
   segundos.querySelector('p').textContent = segundo
 
+
+let fundo = document.getElementById('fundo');
+let fundoHoras = document.querySelector('#horas p')
+let fundoMin = document.querySelector('#minutos p')
+let fundoSeg = document.querySelector('#segundos p')
+
+
+if(hora >= 5 && hora < 13){
+  fundo.className = 'dia'
+  fundoHoras.className = 'diaFundo';
+  fundoMin.className = 'diaFundo';
+  fundoSeg.className = 'diaFundo';
+
+}else if(hora >= 13 && hora < 18){
+  fundo.className = 'tarde';
+  fundoHoras.className = 'tardeFundo';
+  fundoMin.className = 'tardeFundo';
+  fundoSeg.className = 'tardeFundo';
+}else if(hora <= 5 && hora > 18){
+  fundo.className = 'noite'
+  
+}
+
+
+
+
 }
 
 setInterval(relogio, 1000);
 
 relogio()
+
